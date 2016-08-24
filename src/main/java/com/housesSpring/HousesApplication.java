fee315d6a5eb2f865f1265510e4f545852cd1837
tomevet <tomevet1987@gmail.com>
2016-08-24 23:23:39 +0200

    Init

---
+++ src/main/java/com/housesSpring/HousesApplication.java
@@ -0,0 +1,12 @@
+package com.housesSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousesApplication.class, args);
+	}
+}
