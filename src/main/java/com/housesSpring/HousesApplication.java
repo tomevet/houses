45f4728f5197e7fc0401857c11f5d8a206d05fc5
tomevet <tomevet1987@gmail.com>
2016-08-25 00:11:02 +0200

    test2

--- src/main/java/com/housesSpring/HousesApplication.java
+++ src/main/java/com/housesSpring/HousesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HousesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HousesApplication.class, args);
 	}
 }
